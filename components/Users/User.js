7ee632294ef2f6d90513fed53575ae3ef9269e4d
Alex Castillo <108693776+agcast01@users.noreply.github.com>
2024-02-18 11:22:48 -0500

    succesfully fetching and displaying data

--- components/Users/User.js
+++ components/Users/User.js
@@ -1,14 +1,25 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Text, View, FlatList} from 'react-native'
 
+const getUsers = async () => {
+  const response = await fetch('http://192.168.1.218:3000/')
+  const json = await response.json()
+  return json
+}
+
 export default function User() {
-  const {users, setUsers} = useState([])
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    getUsers().then(res => setUsers(res))
+    console.log(users)
+  }, [])
 
   return (
     <View>
       <Text>Users</Text>
       <FlatList 
         data={users}
+        renderItem={({item}) => <Text>{item.email}</Text>}
       />
     </View>
   )

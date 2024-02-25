f14d03b95e2263a196f2dbdaf290c56ca4b21eaa
Alex Castillo <108693776+agcast01@users.noreply.github.com>
2024-02-25 12:34:54 -0500

    added redux

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import User from './components/Users/User';
+import { Provider } from 'react-redux';
+import store from './state/store'
 
 export default function App() {
   return (
-    <View style={styles.container}>      
-      <StatusBar style="auto" />
-      <User />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>      
+        <StatusBar style="auto" />
+        <User />
+      </View>
+    </Provider>
   );
 }
 
---
+++ state/user.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: null,
+  reducers: {
+  }
+})
+
+export default userSlice.reducer

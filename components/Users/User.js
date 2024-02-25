3a904b829473c91f5d2d7918adcac389a25c99b6
Alex Castillo <108693776+agcast01@users.noreply.github.com>
2024-02-25 12:24:40 -0500

    successfully rendering user

--- components/Users/User.js
+++ components/Users/User.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { Text, View, FlatList, StyleSheet } from 'react-native'
+import { Text, View, FlatList, StyleSheet, Button } from 'react-native'
 
 const getUsers = async () => {
-  const response = await fetch('http://192.168.1.218:3000/')
+  const response = await fetch('http://172.19.224.1:3000/users/')
   const json = await response.json()
   return json
 }
 
 export default function User() {
   const [users, setUsers] = useState([])
+  const [buttonColor, setButtonColor] = useState(true)
   useEffect(() => {
     getUsers().then(res => setUsers(res))
-  }, [])
+  }, [buttonColor])
+
+  
 
   return (
     <View>
@@ -21,6 +24,12 @@ export default function User() {
         renderItem={({ item }) => <Text style={styles.item}>{item.email}</Text>}
         style={styles.list}
       />
+      <Button 
+        title="Update State" 
+        style={styles.button}
+        color={buttonColor? "blue": "red"}
+        onPress={() => setButtonColor(!buttonColor)}
+        />
     </View>
   )
 }
